358849259972f7c697c97abd05e94a152df0a55f
raprap <oloresraprap@gmail.com>
2016-01-14 19:56:52 -0800

    Exercise

--- app/src/main/java/sidespell/tech/viewholderquiz/fragments/ListViewFragment.java
+++ app/src/main/java/sidespell/tech/viewholderquiz/fragments/ListViewFragment.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import sidespell.tech.viewholderquiz.R;
 
@@ -13,6 +20,9 @@ import sidespell.tech.viewholderquiz.R;
  */
 public class ListViewFragment extends Fragment {
 
+    private ListView mListView;
+    private ArrayAdapter<String>  listAdapter;
+
     public static ListViewFragment newInstance() {
         return new ListViewFragment();
     }
@@ -20,6 +30,14 @@ public class ListViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_listview, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_listview,container,false);
+
+        mListView.setAdapter(listAdapter);
+
+
+        return view;
     }
+
 }
+
